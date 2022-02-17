a6464c4469efecc43d5a980c42f0c8a945c58f1e
Akira Komamura <akira.komamura@gmail.com>
2022-02-17 19:08:42 +0900

    chore: Add nomake

---
+++ .nomake/flake.nix
@@ -0,0 +1,49 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    async = {
+      flake = false;
+      owner = "jwiegley";
+      repo = "emacs-async";
+      type = "github";
+    };
+    dash = {
+      flake = false;
+      owner = "magnars";
+      repo = "dash.el";
+      type = "github";
+    };
+    "f" = {
+      flake = false;
+      owner = "rejeep";
+      repo = "f.el";
+      type = "github";
+    };
+    org-download = {
+      flake = false;
+      owner = "abo-abo";
+      repo = "org-download";
+      type = "github";
+    };
+    org-volume = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-volume";
+      type = "github";
+    };
+    request = {
+      flake = false;
+      owner = "tkf";
+      repo = "emacs-request";
+      type = "github";
+    };
+    "s" = {
+      flake = false;
+      owner = "magnars";
+      repo = "s.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
