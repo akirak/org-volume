58629974a505aa3d78ea168f0a1546ea23cbf37d
Akira Komamura <akira.komamura@gmail.com>
2022-04-05 01:10:56 +0900

    chore: Update the copyright year

--- org-volume.el
+++ org-volume.el
@@ -1,6 +1,6 @@
 ;;; org-volume.el --- Retrieve metadata of volumes from inside Org -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021 Akira Komamura
+;; Copyright (C) 2021-2022 Akira Komamura
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
 ;; Version: 0.1
